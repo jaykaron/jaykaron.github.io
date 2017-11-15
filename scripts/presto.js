a10265aa532117fedeb1a4af93b963aaf33c726c
jaykaron <jaykaron@gmail.com>
2017-11-15 13:40:22 -0500

    Presto Upgrade
    - presto tags no longer need an id
    - a class on the presto tag is added to the component

--- scripts/presto.js
+++ scripts/presto.js
@@ -15,10 +15,16 @@ function addComponent(sourceFile, anchor, info, component)
     loadComponent("../../../components/"+sourceFile, anchor, info);
     return;
   }
+  component = $.parseHTML(component);
+  $(anchor).replaceWith(component);
+  component = $(component);   // finds the component in the DOM
+  console.log(anchor.className);
+  component.addClass(anchor.className);
   
-  for(var i=0; i<info.length; i++)
-    component = component.replace("TEMP",info[i]);
-  $("#"+anchor.id).replaceWith(component);
+  for(var i=0; i<info.length; i++) {
+    spanToReplace = component.find("#holder"+i);
+    spanToReplace.replaceWith(info[i]);
+  }
 }
 
 function loadComponent(sourceFile, anchor, info){
