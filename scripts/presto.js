7713a4a7a0266ff23e5ddf26ad23d75ff164199f
jaykaron <jaykaron@gmail.com>
2017-11-19 19:42:57 -0500

    Presto Upgrade
    Anything inside <presto> tags is added to the component instead of the <center> tag when rendered.

--- scripts/presto.js
+++ scripts/presto.js
@@ -1,38 +1,49 @@
-var toRender = $("presto");
-for(var i=0; i<toRender.length; i++)
-  render(toRender[i]);
+var anchors = $("presto");
+for(var i=0; i<anchors.length; i++)
+  render(anchors[i]);
 
-function render(anchor){
-  var text = []
+function render(anchor) {
+  // Extracts info from the anchor and passes it to addComponent
+  
+  var children = $(anchor).children();
+  
+  var text = [];
   if(anchor.getAttribute("p-text"))
     text = anchor.getAttribute("p-text").split("|");
+  
   var type = anchor.getAttribute("p-type")
-  addComponent(type, anchor, text, null);
+  
+  addComponent(type, anchor, children, text, null);
 }
 
-function addComponent(type, anchor, info, component)
+function addComponent(type, anchor, children, text, component)
 {
-  if(!component) {
-    loadComponent(type, anchor, info);
+  if(!component) {  // loop until component loads
+    loadComponent(type, anchor, children, text);
     return;
   }
+  
+  // replace <presto>
   component = $.parseHTML(component);
   $(anchor).replaceWith(component);
+  
   component = $(component);   // finds the component in the DOM
+
+  component.find("center").replaceWith(children);
   component.addClass(anchor.className);
   
-  for(var i=0; i<info.length; i++) {
+  for(var i=0; i<text.length; i++) {
     spanToReplace = component.find("#holder"+i);
-    spanToReplace.replaceWith(info[i]);
+    spanToReplace.replaceWith(text[i]);
   }
   
   if( type == "nav")
     navAddActive(anchor.getAttribute("p-activeI"));
 }
 
-function loadComponent(type, anchor, info){
-  $.get("../../../components/"+type+".html", function(p) {
-    addComponent(type, anchor, info, p); 
+function loadComponent(type, anchor, children, text){
+  $.get("../../../components/"+type+".html", function(comp) {
+    addComponent(type, anchor, children, text, comp); 
   }, "html");
 }
 
