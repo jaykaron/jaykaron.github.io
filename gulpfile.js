abaa6179a9cb07d9ef4b9878d78e624a6f4c37ac
jaykaron <jaykaron@gmail.com>
2022-03-27 22:45:52 -0400

    Update markdown gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,11 @@ var markdown = require('gulp-markdown');
 var wrap = require("gulp-wrap");
 const htmlmin = require('gulp-htmlmin');
 
-let rootDir = process.cwd();
-let templateDir = rootDir + "/src/templates";
-
 // Static server
 function browserSyncServer(cb) {
   browserSync.init({
     server: {
-      baseDir: rootDir + "/build"
+      baseDir: "./build"
     }
   });
 }
@@ -25,9 +22,9 @@ const htmlMinConfig = {
   removeComments: true
 }
 
-const build = gulp.parallel(pages, media, stylesheets);
+const build = gulp.parallel(gatherPages, gatherMedia, gatherStylesheets, gatherMarkdown);
 
-function pages(cb) {
+function gatherPages(cb) {
   // Gets .html and .nunjucks files in pages
   return gulp.src('src/pages/**/*.+(html|nunjucks|njk)')
     // Renders template with nunjucks
@@ -38,45 +35,39 @@ function pages(cb) {
     .pipe(gulp.dest('./build'));
 } 
 
-function stylesheets(cb) {
+function gatherStylesheets(cb) {
+  // place stylesheets relative to their location in src/
+  // so src/pages/projects/stylesheets/private.css --> build/projects/stylesheets/private.css
   return gulp.src("./src/**/stylesheets/**/*.css", { base: "./src/pages" })
     .pipe(gulp.dest("./build"))
 }
 
-function buildMarkdown(cb) {
-  return gulp.src('../pages/**/*.md')
-    .pipe(frontMatter({ remove: true }))
-    .pipe(markdown())
-    .pipe(wrap({ src: 'markdown.njk' }, {}, { engine: 'nunjucks' }))
-    .pipe(htmlmin(htmlMinConfig))
-    .pipe(gulp.dest('../../build'));
-}
-
-function media(cb) {
+function gatherMedia(cb) {
   return gulp.src("./**/media/**")
     // all media goes into a single folder
+    // they should always be referenced /media/file
     .pipe(flatten())
     .pipe(gulp.dest("./build/media"))
 }
 
-function watch(cb) {
-  browserSyncServer(cb);
-  gulp.watch('src/(pages|templates)/**/*.+(html|nunjucks|njk)', build);
-  gulp.watch('build/*.html').on('change', reload);
+function gatherMarkdown(cb) {
+  return gulp.src('./src/**/*.md', { base: "./src/pages" })
+    .pipe(frontMatter({ remove: true }))
+    .pipe(markdown())
+    // insert the rendered markdown into the nunjucks template
+    .pipe(wrap({ src: './src/templates/markdown.njk' }))
+    .pipe(nunjucksRender({
+      path: ['src/templates']
+    }))
+    .pipe(htmlmin(htmlMinConfig))
+    .pipe(gulp.dest('./build'));
 }
 
-function watchMarkdown(cb) {
+function watch(cb) {
   browserSyncServer(cb);
-  process.chdir(templateDir)
-  gulp.watch('../pages/**/*.md', buildMarkdown);
-  gulp.watch('../../**/*.html').on('change', () => { return reload(); });
+  gulp.watch('src/**/*', build);
+  gulp.watch('build/**/*').on('change', reload);
 }
 
 exports.build = build;
 exports.watch = watch;
-exports.buildMarkdown = () => {
-  process.chdir(templateDir);
-  return buildMarkdown();
-}
-exports.watchMarkdown = watchMarkdown
-
